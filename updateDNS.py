22bae8d8197aecb2cfdbe67dbcfa3ffa45dbf53e
cbras1980 <cbras1980@gmail.com>
2019-05-28 19:07:15 +0200

    Updating comments

--- updateDNS.py
+++ updateDNS.py
@@ -10,8 +10,8 @@ token='token'
 domain='example.com'
 host='record'
 
-#get records list
 recordid=''
+#check if the record exists for the specified domain in name.com
 resp = requests.get('https://{}/v4/domains/{}/records/'.format(api,domain), auth=(user, token))
 records = (resp.json())['records']
 for record in records:
@@ -21,22 +21,19 @@ for record in records:
       print("Found record ID: {} - Host: {}.{}".format(record['id'],record['host'],record['domainName']))
       break
 
-#If record was not found, exit with error
+#If record was not found, exit with error code 1
 if recordid is '':
   print("Record {}.{} not found. Please create it first.".format(host,domain))
   sys.exit(1)
-#print(json.dumps(resp.json(), indent=4, sort_keys=True))
 
-#get current value for the record
+#get current IP defined for the host in name.com
 resp = requests.get('https://{}/v4/domains/{}/records/{}'.format(api,domain,recordid), auth=(user, token))
 #print(json.dumps(resp.json(), indent=4, sort_keys=True))
 actip = json.loads(json.dumps(resp.json()))['answer']
-#print(actip)
 
 #get external ip address using ipfy.org API
 resp = requests.get('https://api.ipify.org?format=json')
 curip = json.loads(json.dumps(resp.json()))['ip']
-#print(curip)
 
 if actip == curip:
   print("No update needed")
