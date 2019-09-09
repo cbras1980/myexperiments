a883a97f85661eac0c2fb08d181bd643ba4cdf27
cbras1980 <cbras1980@gmail.com>
2019-09-09 19:22:31 +0200

    Update openRemoteSSH.sh

--- openRemoteSSH.sh
+++ openRemoteSSH.sh
@@ -15,8 +15,8 @@ HIGH=15500
 if [ -f $CONF_FILE ]; then
   . $CONF_FILE
   PORT=$(python -c "import random;print(random.randrange($LOW,$HIGH))")
-  sshpass -p $PASS ssh $USER@$HOST "echo $PORT > $(hostname).txt"
-  sshpass -p $PASS ssh -N -R $PORT:127.0.0.1:22 $USER@$HOST
+  sshpass -p $PASS ssh -y -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $USER@$HOST "echo $PORT > $(hostname).txt"
+  sshpass -p $PASS ssh -y -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -N -R $PORT:127.0.0.1:22 $USER@$HOST
 else
   echo "Config file $CONF_FILE not found"
 fi
