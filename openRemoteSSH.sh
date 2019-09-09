65cc8d11abae51aa676ebb1cda2d1da34a14707a
cbras1980 <cbras1980@gmail.com>
2019-09-09 18:53:06 +0200

    Cleanup openRemoteSSH.sh

--- openRemoteSSH.sh
+++ openRemoteSSH.sh
@@ -3,23 +3,16 @@
 # Create config file with:
 # USER="username"
 # PASS="password"
+# HOST="hostname"
+# Software needed: sshpass, python
 #################################################
 CONF_FILE="/etc/remotessh.conf"
 
-if [ -f $CONF_FILE ];
-then
-
-. $CONF_FILE
-
-PORT=$(python -c "import random;print(random.randrange(15000,15500))")
-
-#trap cleanup SIGINT SIGTERM SIGKILL SIGSTOP
-
-sshpass -p $PASS ssh $HOST "echo $PORT > $(hostname).txt"
-sshpass -p $PASS ssh -N -R $PORT:127.0.0.1:22 $HOST "echo $PORT > $(hostname)"
-
+if [ -f $CONF_FILE ]; then
+  . $CONF_FILE
+  PORT=$(python -c "import random;print(random.randrange(15000,15500))")
+  sshpass -p $PASS ssh $USER@$HOST "echo $PORT > $(hostname).txt"
+  sshpass -p $PASS ssh -N -R $PORT:127.0.0.1:22 $USER@$HOST "echo $PORT > $(hostname)"
 else
-
-echo "Config file $CONF_FILE not found"
-
+  echo "Config file $CONF_FILE not found"
 fi
