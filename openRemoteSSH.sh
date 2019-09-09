7017f399e7e9d06ef5883b6f418d2a5eeeea0fc9
cbras1980 <cbras1980@gmail.com>
2019-09-09 18:55:47 +0200

    Update openRemoteSSH.sh

--- openRemoteSSH.sh
+++ openRemoteSSH.sh
@@ -12,7 +12,7 @@ if [ -f $CONF_FILE ]; then
   . $CONF_FILE
   PORT=$(python -c "import random;print(random.randrange(15000,15500))")
   sshpass -p $PASS ssh $USER@$HOST "echo $PORT > $(hostname).txt"
-  sshpass -p $PASS ssh -N -R $PORT:127.0.0.1:22 $USER@$HOST "echo $PORT > $(hostname)"
+  sshpass -p $PASS ssh -N -R $PORT:127.0.0.1:22 $USER@$HOST
 else
   echo "Config file $CONF_FILE not found"
 fi
