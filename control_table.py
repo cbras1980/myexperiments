a5ce257949156f40a0303dfa044e68494f746859
cbras1980 <cbras1980@gmail.com>
2021-11-06 10:56:14 +0100

    Update control_table.py

--- control_table.py
+++ control_table.py
@@ -13,9 +13,9 @@ parser = argparse.ArgumentParser(prog='control_table',
                                  epilog='Enjoy the program! :)')
 
 parser.add_argument('-u','--up',action='store_true',help="Pull the table UP")
-parser.add_argument('-d','--down',action='store_true',help="Push the table DOWN")
-parser.add_argument('-s','--seconds',action='store',type=int,help="Time in seconds")
-parser.add_argument('-p','--position',action='store',type=int,help="Set table height in position 0 to 15")
+parser.add_argument('-d','--down',action='store_true',help="Push the table DOWN"                                                                                                             )
+parser.add_argument('-s','--seconds',action='store',type=int,help="Time in secon                                                                                                             ds")
+parser.add_argument('-p','--position',action='store',type=int,help="Set table he                                                                                                             ight in position 0 to 15")
 
 args = parser.parse_args()
 
@@ -25,9 +25,35 @@ else:
   stime = 2
 
 #Read table position
-f = open('table_pos','r')
-pos = (int)(f.read().strip())
-f.close()
+try:
+  f = open('table_pos','r')
+  pos = (int)(f.read().strip())
+  f.close()
+except IOError:
+  print("File with position does not exist, resetting...")
+  print("I will put the table all UP, starting in 5 seconds, please BE aware of                                                                                                              any objects preventing the table from moving.")
+  print("Press Ctrl+C to cancel...")
+  count = 5
+  RELAIS_1_GPIO = 2
+  GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+  try:
+    while count >= 0:
+      print(count)
+      time.sleep(1)
+      count = count - 1
+    GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
+    time.sleep(17)
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
+    print("Finished re-setting table")
+    pos = 15
+    f = open('table_pos','w')
+    f.write(str(pos))
+    f.close()
+  except KeyboardInterrupt:
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
+    GPIO.cleanup()
+    print("Cancelled")
+    exit(3)
 
 if args.up:
   print("Table goes UP")
@@ -37,8 +63,10 @@ elif args.down:
   RELAIS_1_GPIO = 3
   print("Table goes DOWN")
   increment = -1
-elif args.position:
+elif args.position is not None:
   targetpos = args.position
+  if targetpos > 15: targetpos = 15
+  if targetpos < 0: targetpos = 0
   if targetpos > pos:
     print("Table goes UP")
     RELAIS_1_GPIO = 2
@@ -51,9 +79,9 @@ elif args.position:
     stime = pos - targetpos
   else:
     print("Table is already at position {}".format(targetpos))
-    exit*(2)
+    exit(2)
 else:
-  print("You have to set a direction UP or DOWN or a target POSITION")
+  print("You have to set a direction UP or DOWN or a target POSITION from 0 to 1                                                                                                             5")
   exit(1)
 
 try:
